1bbf339d822518430b9ae3de0eecf510a31fce3b
moin2672 <moin2672@gmail.com>
2018-05-02 14:58:44 +0530

    first commit part 1

--- syed_praw.py
+++ syed_praw.py
@@ -8,6 +8,15 @@ reddit = praw.Reddit(client_id='jgF2ebrSIHroxw',
 subreddit = reddit.subreddit('python')
 
 
-hot_python = subreddit.hot(limit=10)
+hot_python = subreddit.hot(limit=5)
 for submission in hot_python:
-    print(submission)
+    #print(dir(submission))         # will give all the attributes of the object
+    if not submission.stickied:     #checking the post as not stickied
+        #print(submission.title)     # to get the title of the objects; check it in https://www.reddit.com/r/Python/
+        print('Title: {}, \nup: {}, \ndowns: {}, \nHave we visited: {}'.format(submission.title, submission.ups, submission.downs, submission.visited))
+        print ("****************")
+
+
+# to subscribe and unsubscribe the subreddit python call the below commands
+subreddit.subscribe()
+# subreddit.unsubscribe()
